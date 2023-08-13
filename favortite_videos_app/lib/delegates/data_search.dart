74b171eea6de21291e20e50e9475b4dd38d3638c
Enrico Gollner <enricogollner.tech@gmail.com>
2023-08-13 18:35:52 -0300

    Search sugestions by youtube API

--- favortite_videos_app/lib/delegates/data_search.dart
+++ favortite_videos_app/lib/delegates/data_search.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:favortite_videos_app/app/data/http/http_client.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -39,11 +42,50 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    if (query.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder<List>(
+      future: sugestions(query),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(snapshot.data![index]),
+              leading: const Icon(Icons.play_arrow),
+              onTap: () {
+                query = snapshot.data![index];
+              },
+            );
+          },
+        );
+      },
+    );
   }
 
-  sugestions(String search) async {
-    http.Response response = await http.get(Uri.parse(
-        "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"));
+  Future<List> sugestions(String search) async {
+    final client = HttpClient();
+
+    http.Response response = await client.get(
+      url:
+          "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json",
+    );
+
+    if (response.statusCode == 200) {
+      return json
+          .decode(response.body)[1]
+          .map(
+            (value) => value[0],
+          )
+          .toList();
+    } else {
+      throw Exception("Failed to load sugestions");
+    }
   }
 }
